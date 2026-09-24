53e57da8c76e6fda7636f75983893a44c79a1212
Yeongjae Park <62601806+youngpark-POS@users.noreply.github.com>
2026-09-24 16:50:30 +0900

    Grant Lambda event source deployment permissions

--- infra/terraform/serverless-temp/bootstrap/main.tf
+++ infra/terraform/serverless-temp/bootstrap/main.tf
@@ -93,6 +93,17 @@ data "aws_iam_policy_document" "github_deploy" {
     resources = ["arn:aws:lambda:${var.aws_region}:${local.account_id}:function:tarot-temp-*"]
   }
   statement {
+    actions = [
+      "lambda:CreateEventSourceMapping", "lambda:GetEventSourceMapping",
+      "lambda:UpdateEventSourceMapping", "lambda:DeleteEventSourceMapping"
+    ]
+    resources = ["arn:aws:lambda:${var.aws_region}:${local.account_id}:event-source-mapping:*"]
+  }
+  statement {
+    actions   = ["lambda:ListEventSourceMappings"]
+    resources = ["*"]
+  }
+  statement {
     actions   = ["dynamodb:*"]
     resources = ["arn:aws:dynamodb:${var.aws_region}:${local.account_id}:table/tarot-temp-*"]
   }
