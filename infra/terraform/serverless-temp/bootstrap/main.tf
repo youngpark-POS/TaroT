28c45b3c859baa20655e71bb67babfe237ab7872
Yeongjae Park <62601806+youngpark-POS@users.noreply.github.com>
2026-09-24 16:30:26 +0900

    Allow S3 acceleration state inspection

--- infra/terraform/serverless-temp/bootstrap/main.tf
+++ infra/terraform/serverless-temp/bootstrap/main.tf
@@ -71,7 +71,8 @@ data "aws_iam_policy_document" "github_deploy" {
   statement {
     actions = [
       "s3:CreateBucket", "s3:DeleteBucket", "s3:ListBucket", "s3:ListBucketVersions",
-      "s3:GetBucket*", "s3:GetLifecycleConfiguration", "s3:GetReplicationConfiguration",
+      "s3:GetBucket*", "s3:GetAccelerateConfiguration",
+      "s3:GetLifecycleConfiguration", "s3:GetReplicationConfiguration",
       "s3:GetEncryptionConfiguration", "s3:PutBucketPolicy", "s3:DeleteBucketPolicy",
       "s3:PutBucketVersioning", "s3:PutEncryptionConfiguration",
       "s3:PutBucketPublicAccessBlock", "s3:PutBucketTagging"
